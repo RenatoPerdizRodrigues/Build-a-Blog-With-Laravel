036fcbbcc2b5a5a9ec0b59a277eb6cf4fc67e6cb
Renato Perdiz Rodrigues <renato.rodrigues10@fatec.sp.gov.br>
2018-10-25 18:04:55 -0300

    Added Hipster Ipsum for seeding, fixed blog.single view bug

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,10 +12,31 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
+        /** Code used for plain Lorem Ipsum posts
         for ($i = 0; $i <= 10; $i++){
             Post::insert([
                 'title' => simplexml_load_file('http://www.lipsum.com/feed/xml?amount=1&what=words&start=0')->lipsum,
                 'body' => file_get_contents('http://loripsum.net/api/3/medium/plaintext'),
+                'slug' => str_random(3).'-'.str_random(5).'-'.str_random(3)
+            ]);
+        }
+        */
+
+        //Code used for Hipster Ipsum formatted to PHP
+        for ($i = 0; $i <= 10; $i++){
+            $title = file_get_contents('http://hipsterjesus.com/api?paras=3&type=hipster-centric&html=false');
+            substr($title, 0, 10);
+            $title = explode(" ", $title);
+            $title[0] = explode("{\"text\":\"", $title[0]);
+
+            $text = file_get_contents('http://hipsterjesus.com/api?paras=3&type=hipster-centric&html=false');
+            substr($text, 9, 1000);
+            $text = explode("\",\"error", $text);
+            $text[0] = explode("{\"text\":\"", $text[0]);
+
+            Post::insert([
+                'title' => ucfirst($title[0][1]),
+                'body' => ucfirst($text[0][1].'.'),
                 'slug' => str_random(3).'-'.str_random(5).'-'.str_random(3)
             ]);
         }
